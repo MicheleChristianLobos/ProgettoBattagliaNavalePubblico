71cf07115d2fe9d4c427dbab0e86ffd5dc131a29
Michele Christian Lobos <lobosmichelechristian@gmail.com>
2024-12-20 14:22:55 +0100

    a

--- js/xAnime.js
+++ js/xAnime.js
@@ -14,3 +14,9 @@ anime({
   easing: 'linear',
   round: 10 // Will round the animated value to 1 decimal
 });
+
+anime({
+  targets: '.tab',
+  translateX: 270,
+  delay: anime.stagger(100, {start: 500}) // delay starts at 500ms then increase by 100ms for each elements.
+});
