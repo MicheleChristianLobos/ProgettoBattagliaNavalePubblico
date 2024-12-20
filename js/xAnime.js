95822b6a2b51cefa29fda7b54b05d5acf73d00fc
Michele Christian Lobos <lobosmichelechristian@gmail.com>
2024-12-20 15:00:53 +0100

    Anime Titolo cool

--- js/xAnime.js
+++ js/xAnime.js
@@ -1,22 +1,39 @@
 //File JS che gestisce le animazioni di animeJS - Miki
 var roundLogEl = document.querySelector('.round-log');
 
+//Prova animazzione
 anime 
 ({
     targets: ".anime",
-    translateX: 250,
+    translateX: [-1000, 0],
+    delay: anime.stagger(0, {start: 50}),
+    duration: 2000,
     easing: 'easeInOutExpo'
 });
 
-anime({
+//Ore
+anime
+({
   targets: ".ore",
   innerHTML: [0, 112],
   easing: 'linear',
   round: 10 // Will round the animated value to 1 decimal
 });
 
-anime({
+//Per la tabella
+anime
+({
   targets: '.tab',
   translateX: 270,
   delay: anime.stagger(100, {start: 500}) // delay starts at 500ms then increase by 100ms for each elements.
 });
+
+//Per la forma sotto il titolo
+anime
+({
+  targets: ".quadratoTitolo",
+  translateX: [-10000, -500],
+  delay: anime.stagger(0, {start: 50}),
+  duration: 1000,
+  easing: 'easeInOutExpo'
+})
