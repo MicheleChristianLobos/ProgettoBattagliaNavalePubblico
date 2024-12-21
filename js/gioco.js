8b8dce0c1b5eb9bf007efa291d6b840bd450ef39
nicolaschianchiitis <snicola.schianchi@itis.pr.it>
2024-12-21 10:01:39 +0100

    getPosition()

--- js/gioco.js
+++ js/gioco.js
@@ -4,6 +4,7 @@ console.log(listaCelle)
 
 let tabelloneG1 = {};
 
+// Memorizzazione tabellone Giocatore 1
 for (var i = 0; i < listaCelle.length / 20; i++) {
     var riga = [];
     for (var j = i * 10; j < i * 10 + 10; j++) {
@@ -16,8 +17,32 @@ for (var i = 0; i < listaCelle.length / 20; i++) {
     tabelloneG1[i] = riga;
 }
 
+let tabelloneG2 = {};
+
+// Memorizzazione tabellone Giocatore 2
+for (var i = listaCelle.length / 20; i < listaCelle.length; i++) {
+    var riga = [];
+    for (var j = (i - 100) * 10; j < (i - 100) * 10 + 10; j++) {
+        console.log("j: " + j);
+        listaCelle[j].addEventListener('click', function () {
+            ctrlPos();
+        });
+        riga.push(listaCelle[j]);
+    }
+    tabelloneG2[i] = riga;
+}
+
 console.log(tabelloneG1);
 
 function ctrlPos() {
     
+}
+
+function getPosition(id) {
+    let coordsFromId = id.replace("c", "").replace(/\-g\d$/, "").split("-");
+    let position = [];
+    coordsFromId.forEach(coord => {
+        position.push(parseInt(coord));
+    });
+    return position;
 }
