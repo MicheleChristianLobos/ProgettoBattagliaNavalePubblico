89534d122667c0a5e67ec0431e9aeebc955d6e63
nicolaschianchiitis <snicola.schianchi@itis.pr.it>
2024-12-24 12:51:39 +0100

    Continuo controllo caselle inserimento navi

--- js/gioco.js
+++ js/gioco.js
@@ -1,6 +1,6 @@
 //File JS che gestisce il gioco direttamente
 let listaCelle = document.getElementsByClassName("square");
-console.log(listaCelle)
+//console.log(listaCelle)
 
 let tabelloneG1 = {};
 
@@ -8,13 +8,66 @@ let tabelloneG1 = {};
 for (var i = 0; i < listaCelle.length / 20; i++) {
     var riga = [];
     for (var j = i * 10; j < i * 10 + 10; j++) {
-        console.log("j: " + j);
+        //console.log("j: " + j);
+        var counter = 1;
+        // Ascoltatore bottoni tabellone G1
         listaCelle[j].addEventListener('click', function () {
-            ctrlPos();
+            let numCaselle = 0;
+            let visible = isVisible("riquadroGuida");
+            let horizontal = isHorizontal("riquadroGuida");
+            let width = document.getElementById("riquadroGuida").style.width;
+            let height = document.getElementById("riquadroGuida").style.height;
+            if (visible) {
+                if (horizontal) {
+                    switch (width) {
+                        case "164px":
+                            numCaselle = 4;
+                            break;
+                        case "128px":
+                            numCaselle = 3;
+                            break;
+                        case "76px":
+                            numCaselle = 2;
+                            break;
+                        case "32px":
+                            numCaselle = 1;
+                            break;
+                        default:
+                            numCaselle = 0;
+                            break;
+                    }
+                } else {
+                    switch (height) {
+                        case "164px":
+                            numCaselle = 4;
+                            break;
+                        case "128px":
+                            numCaselle = 3;
+                            break;
+                        case "76px":
+                            numCaselle = 2;
+                            break;
+                        case "32px":
+                            numCaselle = 1;
+                            break;
+                        default:
+                            numCaselle = 0;
+                            break;
+                    }
+                }
+                if (controllaCelleAdiacenti(numCaselle, horizontal, getPosition(`c${i+1}-${counter}-g1`))) {
+                    alert("ok");
+                } else {
+                    alert("no");
+                }
+            } else {
+                return;
+            }
         });
         riga.push(listaCelle[j]);
+        counter++;
     }
-    tabelloneG1[i] = riga;
+    tabelloneG1[i + 1] = riga;
 }
 
 let tabelloneG2 = {};
@@ -23,17 +76,17 @@ let tabelloneG2 = {};
 for (var i = listaCelle.length / 20; i < listaCelle.length; i++) {
     var riga = [];
     for (var j = (i - 100) * 10; j < (i - 100) * 10 + 10; j++) {
-        console.log("j: " + j);
+        //console.log("j: " + j);
         // errore qua??
-        listaCelle[j].addEventListener('click', function () {
+        /* listaCelle[j].addEventListener('click', function () {
             ctrlPos();
-        });
+        }); */
         riga.push(listaCelle[j]);
     }
-    tabelloneG2[i] = riga;
+    tabelloneG2[i + 1] = riga;
 }
 
-console.log(tabelloneG1);
+//console.log(tabelloneG1);
 
 function ctrlPos() {
     alert('test')
