c0e117c63fa9598db9fb85647dcd97d2801040a2
nicolaschianchiitis <snicola.schianchi@itis.pr.it>
2024-12-26 17:08:18 +0100

    Iniziato inserimento navi nel tabellone (grafica) e corretto controllo celle (ora funziona)

--- js/gioco.js
+++ js/gioco.js
@@ -3,6 +3,12 @@ let listaCelle = document.getElementsByClassName("square");
 
 let tabelloneG1 = {};
 let riga = [];
+let naviDisponibili = {
+    "portaerei": 1,
+    "incrociatori": 2,
+    "torpedinieri": 3,
+    "sommergibili": 4
+};
 
 // Memorizzazione tabellone Giocatore 1
 for (let i = 0; i < listaCelle.length / 20; i++) {
@@ -56,6 +62,7 @@ for (let i = 0; i < listaCelle.length / 20; i++) {
                 }
                 if (controllaCelleAdiacenti(numCaselle, horizontal, getPositionByID(listaCelle[j].parentElement.id))) {
                     alert("ok");
+                    posizionaNave(numCaselle, horizontal, getPositionByID(listaCelle[j].parentElement.id));
                 } else {
                     alert("no");
                 }
@@ -66,6 +73,113 @@ for (let i = 0; i < listaCelle.length / 20; i++) {
         riga.push(listaCelle[j]);
     }
     tabelloneG1[i + 1] = riga;
+}
+
+function posizionaNave(numCaselle, horizontal, posIniziale) {
+    // Riga costante, ciclo la colonna
+    let riga = posIniziale[0];
+    let colonna = posIniziale[1];
+    let cella = document.getElementById(`c${riga}-${colonna}-g1`);
+    let btnCella = cella.firstChild;
+    let imgBtnCella = btnCella.firstChild;
+
+    if (horizontal) {
+        // Impostazione immagine nella cella
+        btnCella.className = btnCella.className.replace("vertical", "horizontal");
+        switch (numCaselle) {
+            case 4:
+                if (naviDisponibili.portaerei > 0) {
+                    imgBtnCella.className = "cella portaerei";
+                    imgBtnCella.src = "/portaerei";
+                    naviDisponibili.portaerei--;
+                } else {
+                    alert("Non ci sono più portaerei disponibili.");
+                }
+                break;
+            case 3:
+                if (naviDisponibili.incrociatori > 0) {
+                    imgBtnCella.className = "cella incrociatore";
+                    imgBtnCella.src = "/incrociatore";
+                    naviDisponibili.incrociatori--;
+                } else {
+                    alert("Non ci sono più incrociatori disponibili.");
+                }
+                break;
+            case 2:
+                if (naviDisponibili.torpedinieri > 0) {
+                    imgBtnCella.className = " cella torpediniere";
+                    imgBtnCella.src = "/torpediniere";
+                    naviDisponibili.torpedinieri--;
+                } else {
+                    alert("Non ci sono più torpedinieri disponibili.");
+                }
+                break;
+            case 1:
+                if (naviDisponibili.sommergibili > 0) {
+                    imgBtnCella.className = "cella sommergibile";
+                    imgBtnCella.src = "/sommergibile";
+                    naviDisponibili.sommergibili--;
+                } else {
+                    alert("Non ci sono più sommergibili disponibili.");
+                }
+                break;
+            default:
+                alert("Si è verificato un errore (numero di celle: 0).");
+        }
+        /* cella.firstChild.firstChild.src = "/portaerei";
+        cella.setAttribute("colspan", 4); */
+        /* for (let i = posIniziale[1] - 1; i < posIniziale[1] - 1 + numCaselle; i++) {
+            tabelloneG1[riga][i].style.backgroundColor = "green";
+        } */
+    } else {
+        // Colonna costante, ciclo la riga
+        /* let colonna = posIniziale[1] - 1;
+        for (let i = posIniziale[0]; i < posIniziale[0] + numCaselle; i++) {
+            tabelloneG1[i][colonna].style.backgroundColor = "green";
+        } */
+        // Impostazione immagine nella cella
+        btnCella.className = btnCella.className.replace("horizontal", "vertical");
+        switch (numCaselle) {
+            case 4:
+                if (naviDisponibili.portaerei > 0) {
+                    imgBtnCella.className = "cella portaerei";
+                    imgBtnCella.src = "/portaerei";
+                    naviDisponibili.portaerei--;
+                } else {
+                    alert("Non ci sono più portaerei disponibili.");
+                }
+                break;
+            case 3:
+                if (naviDisponibili.incrociatori > 0) {
+                    imgBtnCella.className = "cella incrociatore";
+                    imgBtnCella.src = "/incrociatore";
+                    naviDisponibili.incrociatori--;
+                } else {
+                    alert("Non ci sono più incrociatori disponibili.");
+                }
+                break;
+            case 2:
+                if (naviDisponibili.torpedinieri > 0) {
+                    imgBtnCella.className = " cella torpediniere";
+                    imgBtnCella.src = "/torpediniere";
+                    naviDisponibili.torpedinieri--;
+                } else {
+                    alert("Non ci sono più torpedinieri disponibili.");
+                }
+                break;
+            case 1:
+                if (naviDisponibili.sommergibili > 0) {
+                    imgBtnCella.className = "cella sommergibile";
+                    imgBtnCella.src = "/sommergibile";
+                    naviDisponibili.sommergibili--;
+                } else {
+                    alert("Non ci sono più sommergibili disponibili.");
+                }
+                break;
+            default:
+                alert("Si è verificato un errore (numero di celle: 0).");
+        }
+    }
 }
 
 let tabelloneG2 = {};
